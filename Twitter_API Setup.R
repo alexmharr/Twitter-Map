1e5995a289187fbe3cb6be3b1856d5dbc57981e1
Kelvin Ka Ho Li <kelvinli@uw.edu>
2017-03-04 16:38:44 -0800

    first approach on the twitter API

--- Twitter_API Setup.R
+++ Twitter_API Setup.R
@@ -1,26 +1,18 @@
-#install.packages("twitteR")
-#install.packages("ROAuth")
+library(devtools)
+install_github("twitteR", username="geoffjentry")
 library(twitteR)
-library(ROAuth)
-#set working directory to the repo
-# Download "cacert.pem" file
-download.file(url="http://curl.haxx.se/ca/cacert.pem",destfile="cacert.pem")
+library(base64enc)
 
-#create an object "cred" that will save the authenticated object that we can use for later sessions
-cred <- OAuthFactory$new(consumerKey='XXXXXXXXXXXXXXXXXX',
-                         consumerSecret='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-                         requestURL='https://api.twitter.com/oauth/request_token',
-                         accessURL='https://api.twitter.com/oauth/access_token',
-                         authURL='https://api.twitter.com/oauth/authorize')
-save(cred, file="twitter authentication.Rdata")
-#execute the command below, which will redirect you to another page. Copy the pin from that page, then enter the pin
-#into the console where it says enter pin.
-cred$handshake(cainfo="cacert.pem")
-#run the command below and then in the console, say yes when prompted.
-setup_twitter_oauth("Consumer Key", "Consumer_Secret", 
-                    "access_token", "access_token_secret")
-#you should be up and running now and test the following commands out
+api_key <- "xxxxx"
+
+api_secret <- "xxxxx"
+
+token <- "xxxxx"
+
+token_secret <- "xxxxx"
+
+setup_twitter_oauth(api_key,api_secret,token, token_secret)
+
 search.string <- "#nba"
 no.of.tweets <- 100
 tweets <- searchTwitter(search.string, n = no.of.tweets, lang = "en")
-tweets
