0e176b427c6a7533dc75442aab1696ba7cdf1c68
Kelvin Ka Ho Li <kelvinli@uw.edu>
2017-03-09 01:00:11 -0800

    upload what i have so far

--- Twitter_API Setup.R
+++ Twitter_API Setup.R
@@ -1,43 +1,32 @@
 
+library(twitteR)
 library(streamR)
 library(RCurl)
 library(RJSONIO)
 library(stringr)
-#the code below is used to connect the twitter api
 library(ROAuth)
+library(dplyr)
+
+#the code below is used to connect the twitter api
 requestURL <- "https://api.twitter.com/oauth/request_token"
 accessURL <- "https://api.twitter.com/oauth/access_token"
 authURL <- "https://api.twitter.com/oauth/authorize"
 consumerKey <- "APMnFSmvPJ56HLqRlO957IpNa"
 consumerSecret <- "6Ra0pjhVcqQl64ArBrQtY6jUCraqJYrLA9bTs9MmFv7no3V8Zp"
+token <- "2340813607-fJqGczp5N7NhyrcGfoMR8Hx0yEc3UhOO5x4eJOh"
+token_secret <- "x7Ty5DTPxnBnMcXcwy3KP2vX4inB3UnNqsk0h6bjtzJoO"
 
-my_oauth <- OAuthFactory$new(consumerKey = consumerKey,
-                             consumerSecret = consumerSecret,
-                             requestURL = requestURL,
-                             accessURL = accessURL,
-                             authURL = authURL)
 
-my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
-save(my_oauth, file = "my_oauth.Rdata")
-
-tweets_geolocated <- searchTwitter("#", n=100, lang="en", geocode='34.04993,-118.24084,50mi', since="2014-08-20")
+# normal search
+setup_twitter_oauth(consumerKey,consumerSecret,token, token_secret)
+tweets_geolocated <- searchTwitter("#", lang="en", geocode='34.04993,-118.24084,50mi', since="2017-01-01")
 tweets_geoolocated.df <- twListToDF(tweets_geolocated)
-#use to collect tweets base on location
-load("my_oauth.Rdata")
-filterStream(file.name = "tweets.json", # Save tweets in a json file
-             track = c("#"), # Collect tweets mentioning either Affordable Care Act, ACA, or Obamacare
-             language = "en",
-             location  = c(-119, 33, -117, 35),
-             timeout = 60, 
-             oauth = my_oauth) # Use my_oauth file as the OAuth credentials
 
-tweets.df <- parseTweets("tweets.json", simplify = FALSE)
-#extract only the hashtags
-textScrubber <- function(dataframe) {
-  dataframe$text <-  str_extract(dataframe$text,"#\\S+")
-  return(dataframe)
-}
-View(tweets.df)
+#head treads from each region
+trend <- availableTrendLocations()
+worldwide.trend <- getTrends(1)
+big.city.trend <- filter(trend, country == "United States") 
+  see <- getTrends(2357024)
 
-tweets.df <- textScrubber(tweets.df)
+
 
